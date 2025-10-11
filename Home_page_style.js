a3273173768928537657a4dcb4edea7250cd1d87
chelovek7979 <Dyger3579@gmail.com>
2025-10-11 10:42:32 +0300

    свайпер

--- Home_page_style.js
+++ Home_page_style.js
@@ -7,3 +7,31 @@ document.addEventListener("DOMContentLoaded", () => {
     burgerButton.classList.toggle("active");
   });
 });
+
+/* Настройка Свайпера */
+const swiper = new Swiper('.swiper', {
+  
+  
+  loop: true,
+  spaceBetween: 50,
+  speed: 1000,
+  autoplay: {
+  delay: 2000,
+ },
+
+  
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
