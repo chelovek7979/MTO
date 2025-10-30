4e20bc213ec14b64241eb8ea74ed54ce94e568d2
chelovek7979 <Dyger3579@gmail.com>
2025-10-30 23:59:04 +0300

    скринкаст

--- Home-Page-Style.js
+++ Home-Page-Style.js
@@ -32,3 +32,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+   autoplay: {
+   delay: 2000,
+ },
+  speed: 1000,
+  spaceBetween: 50,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
